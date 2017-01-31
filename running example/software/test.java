0e2be4e04b891a6503f51cda5874c876fdfca0a8
Saimir Bala <s41m1r@users.noreply.github.com>
2017-01-31 19:05:14 +0100

    Write test methods to test solution

--- running example/software/test.java
+++ running example/software/test.java
@@ -1,6 +1,18 @@
 class Test{
   static void main(){
     t = methodA();
-    System.out.println(t)
+    System.out.println(t);
+    
+    test1(methodA());
+    test2(methodA());
   }
+  
+  void test1(){
+    //test 1    
+  }
+  
+  void test2(){
+    //test 2    
+  }
+  
 }
