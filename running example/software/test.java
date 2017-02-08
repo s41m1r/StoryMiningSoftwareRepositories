62cc9faa90852109e0cb874a0d899a1a24e0ed74
Saimir Bala <s41m1r@users.noreply.github.com>
2017-02-08 18:47:04 +0100

    Update test.java

--- running example/software/test.java
+++ running example/software/test.java
@@ -19,4 +19,25 @@ class Test{
     //add further test
   }
   
+  void test4(){
+    //add further test
+    
+    assert(t1 == true);
+    assert(t2 == true);
+    assert(t3 == true);
+    assert(t4 == true);
+    assert(t5 == true);
+    assert(t6 == true);
+    assert(t7 == true);
+    assert(t8 == true);
+    assert(t9 == true);
+    assert(t10 == true);
+    assert(t11 == true);
+    assert(t12 == true);
+    assert(t13 == true);
+    assert(t14 == true);
+    assert(t15 == true);
+    assert(t16 == true);
+  }
+  
 }
